85bc8e60e365fa3f29153b48f951224e38ca5937
Alexandre Anastassov <a.anastassov@iaea.org>
2013-08-21 13:17:58 +0200

    initial commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DomainClasses;
+using Pluralsight.DataLayer;
+
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateBlog();
+            Console.Write("That's all folks...");
+            Console.ReadKey();
+        }
+
+        private static void CreateBlog()
+        {
+            var blog = new Blog { BloggerName = "Julie", Title = "EF41 Blog" };
+            var db = new Context();
+            db.Blogs.Add(blog);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ DataLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using DomainClasses;
+using System.Collections.Generic;
+
+namespace Pluralsight.DataLayer
+{
+    public class Context : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+
+}
